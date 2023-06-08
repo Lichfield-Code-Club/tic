f4e022c3402135895453cf96391d225ad07806bc
Mark Pritchett <unixmiddleware@gmail.com>
2023-06-08 17:51:49 +0100

    Show winner on main screen

--- tic.py
+++ tic.py
@@ -8,8 +8,8 @@ flip=1
 end=9
 win=0
 place = [10,10,10,10,10,10,10,10,10]
-pace = [1,10,20,30,40,50,60,70,80,90]
-winner =0
+tiles = [(-200,200),(0,200),(200,200),(-200,0),(0,0),(200,0),(-200,-200),(0,-200),(200,-200)]
+winner = 10
 y=0
 def cross():
   t.pendown()
@@ -31,9 +31,7 @@ def turn():
     nought()
   else:
     cross()
-#  player*=-1
   
-
 t.shape('circle')
 t.penup()
 t.goto(-300,-300)
@@ -61,102 +59,70 @@ t.forward(600)
 t.penup()
 t.goto(0,0)
 t.speed(10)
-#cross()
-#t.setx(t.xcor()+200)
 
 def play():
   global pos
   global end
   global y
-  if pos == 1 and place[0]==10:
-    t.goto(-200,200)
+  global tiles
+  global place
+
+  coord_num = pos -1
+  if place[coord_num] == 10:
+    t.goto(tiles[coord_num])
     turn()
     t.goto(0,0)
-    place[0]=end%2
-    y+=1
-  elif pos == 2 and place[1]==10:
-    t.goto(0,200)
-    turn()
-    t.goto(0,0)
-    place[1]=end%2
-    y+=1
-  elif pos == 3 and place[2]==10:
-    t.goto(200,200)
-    turn()
-    t.goto(0,0)
-    place[2]=end%2
-    y+=1
-  elif pos == 4 and place[3] ==10:
-    t.goto(-200,0)
-    turn()
-    t.goto(0,0)
-    y+=1
-    place[3]=end%2
-  elif pos == 5 and place[4]==10:
-    t.goto(0,0)
-    turn()
-    t.goto(0,0)
-    y+=1
-    place[4]=end%2
-  elif pos == 6 and place[5]==10:
-    t.goto(200,0)
-    turn()
-    t.goto(0,0)
-    y+=1
-    place[5]=end%2
-  elif pos == 7 and place[6]==10:
-    t.goto(-200,-200)
-    turn()
-    t.goto(0,0)
-    y+=1
-    place[6]=end%2
-  elif pos == 8 and place[7] == 10:
-    t.goto(0,-200)
-    turn()
-    t.goto(0,0)
-    y+=1
-    place[7]=end%2
-  elif pos == 9 and place[8] ==10:
-    t.goto(200,-200)
-    turn()
-    t.goto(0,0)
-    y+=1
-    place[8]=end%2
+    place[coord_num] = end % 2
+    y += 1
+    end -= 1
   else:
     print("Sorry, I can't do that. ")
     end+=1
 
-def WinCheck():
+def Checker(player,total):
   global place
   global winner
   global end
   
-  if sum(place[:3])==0 or sum(place[3:6])==0 or sum(place[7:9])==0:
-    winner=2
-    end=2
-  if place[0]+place[3]+place[6]==0 or place[1]+place[4]+place[7]==0  or place[2]+place[5]+place[8]==0 or place[0]+place[4]+place[8]==0 or place[2]+place[4]+place[6]==0:
-    winner=2
-    end=1
-  if sum(place[:3])==3 or sum(place[4:6])==3 or sum(place[7:9])==3:
-    winner=1
-    end=1
-  if place[0]+place[3]+place[6]==3 or place[1]+place[4]+place[7]==3  or place[2]+place[5]+place[8]==3 or place[0]+place[4]+place[8]==3 or place[2]+place[4]+place[6]==3:
-    winner=1
-    end=1
+  if place[0]+place[1]+place[2] == total or place[3]+place[4]+place[5] == total or place[6]+place[7]+place[8] == total: # rows
+    winner=player
+    end=0
+  if place[0]+place[3]+place[6] == total or place[1]+place[4]+place[7] == total or place[2]+place[5]+place[8] == total: # columns
+    winner=player
+    end=0
+  if place[0] + place[4] + place[8] == total or place[2] + place[4] + place[6] == total: # diagonals
+    winner=player
+    end=0
+
+def WinCheck():
+  global winner
+  Checker(1,3)
+  if winner != 1:
+    Checker(0,0)
 
 while end !=0:
-  print('1-9')
-  no = input()
-  pos = int(no)
+  msg = ''
+  pos = 0
+  while pos == 0:
+    print('1-9')
+    no = input()
+    pos = int(no)
+    if pos > 9:
+      pos = 0
+      print('1-9 only')
   if y%2==0:
     flip=1
   else:
     flip =0
   play()
   WinCheck()
-  if winner>=1:
-    print("Player", winner, "wins!")
+  if winner == 1:
+    msg = "Player 1 wins!"
     end = 0
-  else:
-    print("it's a draw.")
+  elif winner == 0:
+    msg = "Player 2 wins!"
+    end = 0
+  elif end == 0:
+    msg = "it's a draw."
+t.write(msg, move=False, align='center', font=('Arial', 32, 'normal')) 
 sleep(5)
